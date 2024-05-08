5960859286873dad1722bb3d6ff3d3e44b08dfb8
Martin Grabina <mgrabina@itba.edu.ar>
2024-05-08 13:39:54 -0300

    add ad

--- apps/web/src/components/Home/Sidebar/index.tsx
+++ apps/web/src/components/Home/Sidebar/index.tsx
@@ -2,8 +2,10 @@ import type { FC } from 'react';
 
 import SignupCard from '@components/Shared/Auth/SignupCard';
 import Footer from '@components/Shared/Footer';
+import { Ad } from '@targecy/sdk';
 import { memo } from 'react';
 import { useProfileStore } from 'src/store/persisted/useProfileStore';
+import { zeroAddress } from 'viem';
 
 import EnableLensManager from './EnableLensManager';
 import RunAds from './RunAds';
@@ -18,6 +20,18 @@ const Sidebar: FC = () => {
 
   return (
     <>
+      <div className="mb-7">
+        <Ad
+          isDemo={true}
+          publisher={zeroAddress}
+          styling={{
+            backgroundColor: 'rgb(61 97 77 / 0.1)',
+            border: '1px solid rgb(61 97 77)',
+            subtitleColor: 'rgb(61 97 77)',
+            titleColor: 'rgb(61 97 77)'
+          }}
+        />
+      </div>
       <RunAds />
       {loggedOut && <SignupCard />}
       {/* {loggedInWithProfile && <HeyMembershipNft />} */}
